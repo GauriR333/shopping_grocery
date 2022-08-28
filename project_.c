def7d5d31c9cbdac9996045b1734a20d61f2beda
Gauri Ragade <gauriragade98@gmail.com>
2022-08-28 18:34:46 +0530

    added main menu
    added customer modification

--- project_.c
+++ project_.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h >
 typedef struct customer{
 	char name[10];
 	int Ph_no;
@@ -13,22 +14,24 @@ typedef struct item{
 	int QTY;
 }item;
 
+void show_menu();
 void get_item_info(item*);
 void get_customer_info(customer*);
 float item_calculation(item);
+void modify_customer_detil();
 void display_bill(customer,item*);
 
+item items[100];
+customer cust;
+int item_counter=0;
+
 void main(){
-	customer cust;
-	get_customer_info(&cust);
-	item items[100];
-    get_item_info(items);
-	display_bill(cust,items);
+	show_menu();
 }
 	
 void get_customer_info( customer *cust){
 	
-	printf("Enter the Customer Details\n\tName : ");
+	printf("\tEnter the Customer Details\n\tName : ");
 	scanf("%s",&cust->name);
 		
 	printf("\tPhone NO.  : ");
@@ -48,11 +51,12 @@ void display_bill(customer cust,item* items){
 	printf("\tAddress : %s",cust.Address);
 	printf("\n\t-----------------------------------------------------------------");
 	printf("\n\n\n\t-----------------------------------------------------------------");
+	printf("\n\n\n\t********************Your chart Bill********************\n");
 	printf("\n\tName\tMRP\tGST\tQTY\tGST Amount\tTotal Amount");
 	printf("\n\t-----------------------------------------------------------------");
 	int i;
 	float all_item_total=0;
-	for(i=0;i<2;i++){
+	for(i=0;i<item_counter;i++){
   			printf("\n\t%s",items[i].Item);
 			printf("\t%d",items[i].MRP);
 			printf("\t%d",items[i].GST);
@@ -69,10 +73,11 @@ void display_bill(customer cust,item* items){
 }
 
 void get_item_info(item* items){
-	int i;
-	int b=2;
+	char choice [1];
 	printf("\n\tEnter Items ");
-	for(i=0;i<b;i++){
+	do{
+		int i=item_counter++;
+		
 		printf("\n\tName : ");
 		scanf("%s",&items[i].Item);
 		
@@ -85,17 +90,115 @@ void get_item_info(item* items){
 		printf("\tQuntity : ");
 		scanf("%d",&items[i].QTY);
 		
-		printf("\n");
-	}
+		printf("\n Do you want to add more items ? (y/n) : ");
+		scanf("%s",&choice);
+	}while(strcmp(choice,"y")==0);
 }
 
 float item_calculation(item item){
 	float gst=((item.MRP*item.GST)/100)*item.QTY;
 	return gst;		
 }
+void show_menu(){
+	
+	printf("\n\t*************************WEL-COME TO SHOPE*************************\n");
+		
+	while(1){
+	
+	int choice;
+	printf("\n\t 1. Enter customer information " );
+	printf("\n\t 2. Add to cart ");
+	printf("\n\t 3. Modify cart ");
+	printf("\n\t 4. Modify customer detil  ");
+	printf("\n\t 5. Show bill ");	
+	printf("\n\t 6. Exit ");
+	printf("\n\t Enter choice : ");
+	scanf("%d",&choice);
+	
+		switch(choice){
+			
+			case 1:
+				get_customer_info(&cust);
+				
+			break;		
+			
+			case 2:
+				
+			 	get_item_info(items);
+			 	
+			break;
+		
+			case 3:
+			
+			break;
+		
+			case 4:
+			modify_customer_detil();
+			
+			break;
+			
+			case 5:
+				display_bill(cust,items);
+	
+			break;
+		
+			case 6:
+			exit(0);
+	//	break;
+		 
+			default:
+			printf("\n\tPlease enter correct choice !");
+				
+		}
+	} 
+	
+}
 
-
-
+void modify_customer_detil(){
+	
+		int choice,i;
+		//int true;
+		choice=i;
+	
+	for(i=0;i<=4;i++){
+	
+		printf("\n\t----\t Here you change information  ----\n");
+		printf("\n\t 1. Chang Name ");
+		printf("\n\t 2. Change phone no. ");
+		printf("\n\t 3. Change Addrees ");
+		printf("\n\t 4. Chages Done ");
+		printf("\n\t Enter choice : ");
+		scanf("%d",&choice);
+			
+		//	if(choice==true){
+			
+					switch(choice){
+				
+					case 1:
+					printf("\n\tName : ");
+					scanf("\n\t%s",&cust.name);
+					break;
+					
+					case 2:
+					printf("\n\tPhone no. : ");
+					scanf("\n\t%d",&cust.Ph_no);
+					break;
+				
+					case 3:
+					printf("\n\tAddress : ");
+					scanf("\n\t%s",&cust.Address);
+					break;
+				
+					case 4:
+					break;
+	
+					//default:
+					//printf("Please enter your choice ?");	
+					}
+		//}else;
+			
+	}
+}
 
 	
 	
